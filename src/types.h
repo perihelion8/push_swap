828404add523bf91d5fa4fa180d281915f683963
perihelion8 <abazzoun540@gmail.com>
2025-08-30 16:55:49 +0300

    2eval

--- src/types.h
+++ src/types.h
@@ -6,12 +6,14 @@
 /*   By: abazzoun <abazzoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 07:45:19 by abazzoun          #+#    #+#             */
-/*   Updated: 2025/08/30 16:51:41 by abazzoun         ###   ########.fr       */
+/*   Updated: 2025/08/30 16:55:05 by abazzoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TYPES_H
 # define TYPES_H
+
+# include <stddef.h>
 
 typedef unsigned int	t_uint;
 typedef unsigned long	t_ulong;
