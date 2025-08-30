63aab8d274fb7a99a7e4e558bfa82c9ed59793c6
perihelion8 <abazzoun540@gmail.com>
2025-08-30 16:57:49 +0300

    some new commits

--- src/list_internal.h
+++ src/list_internal.h
@@ -6,14 +6,13 @@
 /*   By: abazzoun <abazzoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 16:23:46 by abazzoun          #+#    #+#             */
-/*   Updated: 2025/08/26 00:33:15 by abazzoun         ###   ########.fr       */
+/*   Updated: 2025/08/30 16:57:02 by abazzoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIST_INTERNAL_H
 # define LIST_INTERNAL_H
 
-# include <stddef.h>
 # include "types.h"
 # include "list.h"
 
